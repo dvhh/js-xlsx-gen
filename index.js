42753129456ea231f87b56c4b9281782f2e2f746
dvhh <dvhh@yahoo.com>
2018-06-21 20:21:53 +0900

    Revised api ergonomy, handling rename and delete

--- index.js
+++ index.js
@@ -52,10 +52,28 @@
 		element.click();
 	}
 
-	{
-		let list = document.getElementById( "files" );
-		list.addEventListener( "change", function(e) {
-			console.log(e);
+	let setupButtonEventListener = function( targetId, event, handler ) {
+		if( typeof(targetId) == "undefined" ) { return; }
+		let button = document.getElementById(targetId);
+		if( typeof(button) == "undefined" ) { return; }
+		button.addEventListener( event, handler );
+	};
+
+	let disableButton = function ( targetId, disabled ) {
+		if( typeof(targetId) == "undefined" ) { return; }
+		let button = document.getElementById(targetId);
+		if( typeof(button) == "undefined" ) { return; }
+		button.disabled = disabled;
+	};
+
+	let setupList = function (target) {
+		console.log(target.dataset);
+
+		const buttonConvertId    = target.dataset["buttonConvertId"];
+		const buttonDeletefileId = target.dataset["buttonDeletefileId"];
+		const buttonRenamefileId = target.dataset["buttonRenamefileId"];
+
+		target.addEventListener( "change", function(e){
 			let hasSelected = 0;
 			for(let i = 0; i < e.target.options.length; ++i ) {
 				if( e.target.options[i].selected ) {
@@ -63,65 +81,96 @@
 					break;
 				}
 			}
-			document.getElementById( "renameFile" ).disabled = !hasSelected;
-			document.getElementById( "deleteFile" ).disabled = !hasSelected;
-		} );
 
-		console.log(list);
-		// Wire event on "Add file(s)" button
-		document.getElementById( "addFile" ).addEventListener( "click", function( e ) {
-			let element = document.createElement("input");
-			element.type="file";
-			element.multiple="multiple";
-			element.accept=".csv";
-			element.addEventListener( 'change', function( e ) {
-				let files = e.target.files;
-				for( let i=0; i < files.length; ++i ) {
-					let file=files[i];
-					let reader = new FileReader( );
-					reader.onload = function ( e ) {
-						/*contents.push({"name":file.name,"content":e.target.result});
-						if( contents.length == files.length ) {
-							let result = csv2xml(contents);
-							download(result);
-						}*/
-						console.log(e)
-						let option = document.createElement("option");
-						option.innerHTML = file.name;
-						option.value=e.target.result;
-						list.appendChild(option);
-						document.getElementById( "convert" ).disabled=false;
-					}
-					reader.readAsText( file );
+			[
+				buttonDeletefileId,
+				buttonRenamefileId
+			].forEach(
+				function(key) {
+					disableButton( key, !hasSelected )
 				}
-			} );
-			element.style.display="none";
-			document.body.appendChild(element);
-			element.click()
+			);
 		});
 
-		// Wire event on "Convert" button
-		document.getElementById( "convert" ).addEventListener( "click", function( e ) {
-			let contents = new Array();
-			for(let i=0; i < list.options.length; ++i ) {
-				contents.push({
-					"name"    : list.options[i].outerText,
-					"content" : list.options[i].value
-				});
+		setupButtonEventListener(
+			target.dataset["buttonAddfileId"],
+			"click",
+			function ( e ) {
+				let element = document.createElement("input");
+				element.type="file";
+				element.multiple="multiple";
+				element.accept=".csv";
+				element.addEventListener( 'change', function( e ) {
+					let files = e.target.files;
+					for( let i=0; i < files.length; ++i ) {
+						let file=files[i];
+						let reader = new FileReader( );
+						reader.onload = function ( e ) {
+							console.log(e)
+							let option = document.createElement("option");
+							option.innerHTML = file.name;
+							option.value=e.target.result;
+							target.appendChild(option);
+							disableButton( buttonConvertId , false );
+						}
+						reader.readAsText( file );
+					}
+				} );
+				element.style.display="none";
+				document.body.appendChild(element);
+				element.click();
 			}
-			let result = csv2xml(contents);
-			download(result);
-		});
+		);
 
-		// Wire event on "Rename" button
-		document.getElementById( "renameFile" ).addEventListener( "click", function( e ) {
-			for(let i=0; i < list.options.length; ++i ) {
-				if(list.option) {}
+		setupButtonEventListener(
+			buttonConvertId,
+			"click",
+			function( e ) {
+				let contents = new Array();
+				for(let i=0; i < target.options.length; ++i ) {
+					contents.push({
+						"name"    : target.options[i].outerText,
+						"content" : target.options[i].value
+					});
+				}
+				let result = csv2xml(contents);
+				download(result);
 			}
-		});
-	}
+		);
+
+		setupButtonEventListener(
+			buttonDeletefileId,
+			"click",
+			function( e ) {
+				let toRemove = new Array();
+				for(let i = 0; i < target.options.length; ++i ) {
+					if( target.options[i].selected && window.confirm("delete '"+ target.options[i].outerText + "' ?" ) ) {
+						toRemove.push(target.options[i]);
+					}
+				}
+				toRemove.forEach( function(element) { target.removeChild(element); });
+			}
+		);
+
+		setupButtonEventListener(
+			buttonRenamefileId,
+			"click",
+			function( e ) {
+				for(let i = 0; i < target.options.length; ++i ) {
+					if( target.options[i].selected  ) {
+						target.options[i].innerHTML = window.prompt("rename '" + target.options[i].outerText + "' to ?", target.options[i].outerText );
+					}
+				}
+			}
+		);
+	};
 
 	let targets = document.getElementsByClassName("csvConverter");
-	//for(let i=0;i < )
-	document.getElementsByTagName("div")[0].style.display="block";
+	console.log(targets);
+
+	for( let i=0;i < targets.length; ++i ) {
+		setupList(targets[i])
+	}
+
+	document.getElementsByTagName("div")[0].style.display = "block";
 })()
